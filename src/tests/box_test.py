ae63adfe0ca613326401f698dc5e1f0257538b31
Andre Jonas <nipsky@googlemail.com>
2025-03-20 02:05:49 +0100

    Add tests for merging overlapping OCR boxes in box_test.py

--- src/tests/box_test.py
+++ src/tests/box_test.py
@@ -47,3 +47,38 @@ def test_merge_boxes2():
     assert page.layout[0].y == 0
     assert page.layout[0].width == 50
     assert page.layout[0].height == 150
+
+def test_merge_boxes_overlapping1():
+    page = Page(image_path, ocr_processor=OCRProcessor(project_settings))
+    box1 = OCRBox(x=0, y=0, width=50, height=50)
+    box2 = OCRBox(x=0, y=25, width=50, height=50)
+
+    page.layout.add_ocr_box(box1)
+    page.layout.add_ocr_box(box2)
+
+    page.merge_ocr_box_with_ocr_box(0, 1)
+
+    assert len(page.layout) == 1
+    assert page.layout[0].x == 0
+    assert page.layout[0].y == 0
+    assert page.layout[0].width == 50
+    assert page.layout[0].height == 75
+
+def test_merge_boxes_overlapping2():
+    page = Page(image_path, ocr_processor=OCRProcessor(project_settings))
+    box1 = OCRBox(x=0, y=0, width=50, height=50)
+    box2 = OCRBox(x=0, y=25, width=50, height=50)
+    box3 = OCRBox(x=0, y=75, width=50, height=50)
+
+    page.layout.add_ocr_box(box1)
+    page.layout.add_ocr_box(box2)
+    page.layout.add_ocr_box(box3)
+
+    page.merge_ocr_box_with_ocr_box(0, 1)
+    page.merge_ocr_box_with_ocr_box(0, 1)
+
+    assert len(page.layout) == 1
+    assert page.layout[0].x == 0
+    assert page.layout[0].y == 0
+    assert page.layout[0].width == 50
+    assert page.layout[0].height == 125
